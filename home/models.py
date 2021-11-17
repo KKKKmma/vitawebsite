675b0ac26a819accd3a8ce22e530247aed824e14
KKKKmma <cat1437fish@gmail.com>
2021-11-17 16:05:13 +0800

    creat database - table

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models.fields import DateField
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length = 30,null=False)
+    phone_number = models.IntegerField(null=False,unique=True)
+    email = models.EmailField(null=False)
+    payment_state = models.BooleanField(default=False)
+    order_number = models.IntegerField(max_length=8)
+
+    def __unicode__(self):
+        return self.username
+
+
+
+
+
+
+
+
+    
--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import DateField
 
 # Create your models here.
+class Merchandise(models.Model):
+    merchandise_number = models.CharField(max_length=8,primary_key=True)
+    reservation_date =  models.DateField(auto_now=False, auto_now_add=False)
+    reservation_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    stock = models.BooleanField(default=True)
