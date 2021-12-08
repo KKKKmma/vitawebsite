8adef4b91e94e7e3afa07fb619045672a3bb294b
KKKKmma <cat1437fish@gmail.com>
2021-12-08 15:03:17 +0800

    update api setting

--- vitawebsite/settings.py
+++ vitawebsite/settings.py
@@ -52,12 +52,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# 解決跨域問題
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'vitawebsite.urls'
 # ROOT_URLCONF = 'django_rest_framework_tutorial.urls'
